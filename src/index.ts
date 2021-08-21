b710ec5155f22ca6bd5bae9be6ff436db3e0e53d
jquinzell <>
2021-08-21 11:31:28 -0500

    unused import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { createNextSprint } from './sprint'
 import * as cron from 'node-cron'
 import { getActionableTasks } from './tasks'
-import { notion } from './notion'
 
 cron.schedule('0 0 * * *', () => {
   createNextSprint().catch(console.error)
